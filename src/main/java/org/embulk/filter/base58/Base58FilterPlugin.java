3d264e0987b74ee571a70d2de3ac67c61bed4021
Kevin Fitzgerald <kevin@kevinfitzgerald.net>
2016-07-15 19:52:44 -0500

    Changed the functionality of `new_name` to add a new column instead of change the name of the original column  * Also added logging

--- src/main/java/org/embulk/filter/base58/Base58FilterPlugin.java
+++ src/main/java/org/embulk/filter/base58/Base58FilterPlugin.java
@@ -2,6 +2,7 @@ package org.embulk.filter.base58;
 
 import com.google.common.base.Optional;
 import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
 import org.embulk.config.Config;
 import org.embulk.config.ConfigDefault;
 import org.embulk.config.ConfigSource;
@@ -16,15 +17,16 @@ import org.embulk.spi.PageBuilder;
 import org.embulk.spi.PageOutput;
 import org.embulk.spi.PageReader;
 import org.embulk.spi.Schema;
-import org.embulk.spi.time.Timestamp;
 import org.embulk.spi.type.Types;
-import org.msgpack.value.Value;
+import org.slf4j.Logger;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Base58FilterPlugin implements FilterPlugin {
+
+    private final Logger logger = Exec.getLogger(Base58FilterPlugin.class);
 
     public interface PluginTask extends Task {
         @Config("columns")
@@ -52,20 +54,29 @@ public class Base58FilterPlugin implements FilterPlugin {
     public void transaction(ConfigSource config, Schema inputSchema, FilterPlugin.Control control) {
 
         PluginTask task = config.loadConfig(PluginTask.class);
-        Map<String, Base58Column> base58ColumnMap = convertBase58ColumnListToMap(task.getColumns());
+        ImmutableList.Builder<Column> builder = ImmutableList.builder();
 
-        Schema.Builder builder = Schema.builder();
+        // Roll through original columns
+        int i = 0;
         for (Column column : inputSchema.getColumns()) {
+            builder.add(new Column(i++, column.getName(), column.getType()));
+        }
 
-            Base58Column base58Column = base58ColumnMap.get(column.getName());
-
-            if (base58Column != null) {
-                builder.add(base58Column.getNewName().or(column.getName()), Types.STRING);
+        // Append new columns, if base58 columns desire it
+        for (Base58Column column : task.getColumns()) {
+            if (column.getNewName().isPresent()) {
+                logger.info("added column: name: {}, type: {}, index: {}",
+                        column.getNewName().get(),
+                        Types.STRING,
+                        i);
+                builder.add(new Column(i++, column.getNewName().get(), Types.STRING));
             } else {
-                builder.add(column.getName(), column.getType());
+                logger.info("overriding column: name: {}", column.getName());
             }
         }
-        control.run(task.dump(), builder.build());
+
+        Schema outputSchema = new Schema(builder.build());
+        control.run(task.dump(), outputSchema);
     }
 
     @Override
@@ -90,48 +101,86 @@ public class Base58FilterPlugin implements FilterPlugin {
             }
 
             private void setValue() {
-                for (Column inputColumn : inputSchema.getColumns()) {
-                    if (reader.isNull(inputColumn)) {
-                        builder.setNull(inputColumn);
+
+                // Do base58 conversions ahead of iterating output columns
+                final Map<String, String> base58OutputMap = new HashMap<>();;
+                for (Base58Column column : base58ColumnMap.values()) {
+                    String inputValue, convertedValue = null;
+
+                    // If the original column is not a string, then forget about it (it should be hex)
+                    Column originalColumn = outputColumnMap.get(column.getName());
+                    if (Types.STRING.equals(originalColumn.getType())) {
+                        inputValue = reader.getString(originalColumn);
+                    } else {
+                        logger.error("cannot convert base58 value of non-string values. name: {}, type: {}, index: {}",
+                                originalColumn.getName(),
+                                originalColumn.getType(),
+                                originalColumn.getIndex());
+                        throw new DataException("Unexpected string type in column `"+originalColumn.getName()+"`. Got: " + originalColumn.getType());
+                    }
+
+                    // Convert the value
+                    try {
+                        convertedValue = convertValue(inputValue, column.getIsEncode().get(), column.getPrefix().get());
+                    } catch (Exception e) {
+                        // Failed to do the conversion. Probably misconfigured or malformed value
+                        logger.error("failed to encode/decode base58 column value. name: {}, type: {}, index: {}, value: {}, method: {}, prefix: {}, target_name: {}",
+                                originalColumn.getName(),
+                                originalColumn.getType(),
+                                originalColumn.getIndex(),
+                                inputValue,
+                                column.getIsEncode().get() ? "encode" : "decode",
+                                column.getPrefix(),
+                                column.getNewName().or(column.getName()));
+                        // Don't crash the import if a single value is screwed up. Just log it for now
+                    }
+
+                    // Add it to the output mappings
+                    if (column.getNewName().isPresent()) {
+                        base58OutputMap.put(column.getNewName().get(), convertedValue);
+                    } else {
+                        base58OutputMap.put(column.getName(), convertedValue);
+                    }
+                }
+
+                List<Column> columns = outputSchema.getColumns();
+                for (Column outputColumn : columns) {
+
+                    // Did we convert the value for this column?
+                    if (base58OutputMap.containsKey(outputColumn.getName())) {
+                        String value = base58OutputMap.get(outputColumn.getName());
+                        if (value == null) {
+                            builder.setNull(outputColumn);
+                        } else {
+                            builder.setString(outputColumn, base58OutputMap.get(outputColumn.getName()));
+                        }
                         continue;
                     }
 
-                    // Write the original data
-                    Object inputValue;
-                    if (Types.STRING.equals(inputColumn.getType())) {
-                        final String value = reader.getString(inputColumn);
-                        inputValue = value;
-                        builder.setString(inputColumn, value);
-                    } else if (Types.BOOLEAN.equals(inputColumn.getType())) {
-                        final boolean value = reader.getBoolean(inputColumn);
-                        inputValue = value;
-                        builder.setBoolean(inputColumn, value);
-                    } else if (Types.DOUBLE.equals(inputColumn.getType())) {
-                        final double value = reader.getDouble(inputColumn);
-                        inputValue = value;
-                        builder.setDouble(inputColumn, value);
-                    } else if (Types.LONG.equals(inputColumn.getType())) {
-                        final long value = reader.getLong(inputColumn);
-                        inputValue = value;
-                        builder.setLong(inputColumn, value);
-                    } else if (Types.TIMESTAMP.equals(inputColumn.getType())) {
-                        final Timestamp value = reader.getTimestamp(inputColumn);
-                        inputValue = value;
-                        builder.setTimestamp(inputColumn, value);
-                    } else if (Types.JSON.equals(inputColumn.getType())) {
-                        final Value value = reader.getJson(inputColumn);
-                        inputValue = value;
-                        builder.setJson(inputColumn, value);
-                    } else {
-                        throw new DataException("Unexpected type:" + inputColumn.getType());
+                    // No value?
+                    if (reader.isNull(outputColumn)) {
+                        builder.setNull(outputColumn);
+                        continue;
                     }
 
-                    // Overwrite the column if it's base58 column.
-                    Base58Column base58Column = base58ColumnMap.get(inputColumn.getName());
-                    if (base58Column != null) {
-                        final Column outputColumn = outputColumnMap.get(base58Column.getNewName().or(inputColumn.getName()));
-                        final String convertedValue = convertValue(inputValue.toString(), base58Column.getIsEncode().get(), base58Column.getPrefix().get());
-                        builder.setString(outputColumn, convertedValue);
+                    // Inherit value
+                    if (Types.STRING.equals(outputColumn.getType())) {
+                        builder.setString(outputColumn, reader.getString(outputColumn));
+                    }
+                    else if (Types.BOOLEAN.equals(outputColumn.getType())) {
+                        builder.setBoolean(outputColumn, reader.getBoolean(outputColumn));
+                    }
+                    else if (Types.DOUBLE.equals(outputColumn.getType())) {
+                        builder.setDouble(outputColumn, reader.getDouble(outputColumn));
+                    }
+                    else if (Types.LONG.equals(outputColumn.getType())) {
+                        builder.setLong(outputColumn, reader.getLong(outputColumn));
+                    }
+                    else if (Types.TIMESTAMP.equals(outputColumn.getType())) {
+                        builder.setTimestamp(outputColumn, reader.getTimestamp(outputColumn));
+                    }
+                    else if (Types.JSON.equals(outputColumn.getType())) {
+                        builder.setJson(outputColumn, reader.getJson(outputColumn));
                     }
                 }
             }
